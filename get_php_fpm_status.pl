81abb8c59bb2b781497adc07727d127247033794
Elan Ruusamäe <glen@delfi.ee>
2011-01-27 14:56:00 +0200

    import cacti-phpfpmstatus.zip
    first post from:
    http://forums.cacti.net/viewtopic.php?f=12&t=41580
    
    Post subject: PHP-FPM Status template
    Posted: Thu Jan 27, 2011 2:56 pm
    Author: Lucky SB
    
    get_php_fpm_status.pl copy to ../scripts directory
    cacti_graph_template_php-fpm_pool_status.xml import to cacti
    
    Script check url http://server:port/phpfpm-status
    
    Thanks for .xml and idea for Alexander Moskalenko
    http://forum.nginx.org/read.php?25,167900,167900#msg-167900

---
+++ get_php_fpm_status.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl
+use Getopt::Long;
+
+if (! eval "require LWP::UserAgent;")
+{
+	$ret = "LWP::UserAgent not found";
+}
+
+
+my $host = '';         # server host
+my $port = 80;       # tcp port
+my $script = '/phpfpm-status';       # test script (absolute path starting at / - root directory -)
+my $timeout = 3;       # timeout in seconds
+
+# check command line options
+
+GetOptions ('H=s' => \$host, 'p=i' => \$port, 's=s' => \$script, 't=i' => \$timeout);
+
+if (($host eq '') || ($port eq '')) {
+  print "Usage: check_php-cgi.pl -H host -p port [-s <test script>] [-t <timeout seconds>]\n";
+  exit(-1);
+}
+      
+# run check
+
+	my $ua = LWP::UserAgent->new(timeout => 5);
+
+	my $response = $ua->request(HTTP::Request->new('GET','http://'.$host.':'.$port.$script));
+	my @content = split (/\n/, $response->content);
+	my $var_accepted = -1;
+	if ($content[2] =~ /^accepted conn:\s+(\d+)\s*$/i) {
+		$var_accepted = $1;
+	}
+	my $var_active = -1;
+	if ($content[6] =~ /^active processes:\s+(\d+)\s*$/i) {
+		$var_active = $1;
+	}
+	my $var_idle = -1;
+	if ( $content[5] =~ /^idle processes:\s+(\d+)\s*$/i ) {
+		$var_idle = $1;
+	}
+	my $var_total = -1;
+	if ($content[7] =~ /^total processes:\s+(\d+)\s*$/i) {
+		$var_total = $1;
+	}
+print 'accepted:' . $var_accepted . ' idle:' . $var_idle . ' active:'. $var_active . ' total:' . $var_total . "\n";
+
+
+

d4b839a96fbe024ff721233fc913280bdf7e7454
Elan Ruusamäe <glen@delfi.ee>
2013-07-04 00:25:37 +0300

    reorganize: move data fetching to function

--- get_php_fpm_status.pl
+++ get_php_fpm_status.pl
@@ -20,37 +20,47 @@
 use strict;
 use warnings;
 use Getopt::Long;
-use LWP::UserAgent;
 
 # command line parameters with some defaults
 my $host = '';         # server host
 my $port = 80;       # tcp port
-my $script = '/fpm-status';       # test script (absolute path starting at / - root directory -)
+my $script = '/fpm-status';       # script (absolute path starting at / - root directory -)
 my $timeout = 3;       # timeout in seconds
 
 # check command line options
-GetOptions ('H=s' => \$host, 'p=i' => \$port, 's=s' => \$script, 't=i' => \$timeout);
+GetOptions(
+	'H=s' => \$host,
+	'p=i' => \$port,
+	's=s' => \$script,
+	't=i' => \$timeout,
+);
 
-if (($host eq '') || ($port eq '')) {
-	print "Usage: check_php-cgi.pl -H host -p port [-s <test script>] [-t <timeout seconds>]\n";
+if (!$host || !$port) {
+	print "Usage: check_php-cgi.pl -H host -p port [-s <script path>] [-t <timeout seconds>]\n";
 	exit(-1);
 }
- 
-# run check
-my $ua = LWP::UserAgent->new;
 
-$ua->timeout($timeout);
-$ua->default_header('Host' => $host);
+# retrieve PHP-FPM status over HTTP protocol
+sub get_data_http {
+	use LWP::UserAgent;
+	my $ua = LWP::UserAgent->new;
 
-my $url = 'http://'.$host.':'.$port.$script;
-my $response = $ua->request(HTTP::Request->new('GET', $url));
-unless ($response->is_success) {
-	die "GET $url: ". $response->message;
+	$ua->timeout($timeout);
+	$ua->default_header('Host' => $host);
+
+	my $url = 'http://'.$host.':'.$port.$script;
+	my $response = $ua->request(HTTP::Request->new('GET', $url));
+	unless ($response->is_success) {
+		die "GET $url: ". $response->message;
+	}
+	return $response->content;
 }
+
+my $content = get_data_http();
 
 # parse response
 my %data;
-foreach my $line (split /\n/, $response->content) {
+foreach my $line (split /\n/, $content) {
 	next unless (my($key, $value) = $line =~ /^([^:]+)\s*:\s*(\S+)/);
 	$data{$key} = $value;
 }
@@ -62,8 +72,9 @@ my %keys = (
 	'idle processes' => 'idle',
 	'total processes' => 'total',
 );
+my $out = '';
 while (my($key, $tag) = each %keys) {
 	my $value = defined $data{$key} ? $data{$key} : -1;
-	printf("%s:%s ", $tag, $value);
+	$out .= sprintf("%s:%s ", $tag, $value);
 }
-print "\n";
+print $out, "\n";
